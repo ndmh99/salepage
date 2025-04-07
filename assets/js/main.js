b968344550b92f876dd8b796c0cd27bf5a388430
Doan Minh Hieu Nguyen <66507103+ndmh99@users.noreply.github.com>
2025-04-07 03:29:49 -0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/**
+
+	HTML Landing Pages Pack
+
+  - Version: 1.0
+
+  - 2021
+  
+  - Author: fronthemes
+  
+  - https://fronthemes.com
+
+ */
+
+ /*=========================================
+ =            JAVASCRIPT - MAIN            =
+ =========================================*/
+ 
+// Hide collapsible navbar on click
+$('.nav-link').on('click', function () {
+  $('.navbar-collapse').collapse('hide');
+});
